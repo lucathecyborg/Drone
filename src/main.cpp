b4cdb5b07018cb7027427624f7efa860d4f2bfd1
lucathecyborg <lucaborg21a@gmail.com>
2026-01-25 22:56:21 +0100

    added  motor init and variables

--- src/main.cpp
+++ src/main.cpp
@@ -6,6 +6,78 @@
 #include "BMP.h"
 #include "ADS1115.h"
 
+// PWM config for motors
+#define TOPL_PIN 14
+#define TOPR_PIN 27
+#define BOTTOML_PIN 26
+#define BOTTOMR_PIN 25
+
+#define TOPL_CHANNEL 0
+#define TOPR_CHANNEL 1
+#define BOTTOML_CHANNEL 2
+#define BOTTOMR_CHANNEL 3
+
+#define PWM_FREQ 50       // 50Hz for ESC
+#define PWM_RESOLUTION 16 // 16-bit resolution
+
+int initMotors()
+{
+  Serial.println("initin motors...");
+  // Setup PWM for all ESCs
+  if (ledcSetup(TOPL_CHANNEL, PWM_FREQ, PWM_RESOLUTION) == 0)
+  {
+    Serial.println("Failed to setup TOPL channel");
+    return -1;
+  }
+  ledcAttachPin(TOPL_PIN, TOPL_CHANNEL);
+
+  if (ledcSetup(TOPR_CHANNEL, PWM_FREQ, PWM_RESOLUTION) == 0)
+  {
+    Serial.println("Failed to setup TOPR channel");
+    return -1;
+  }
+  ledcAttachPin(TOPR_PIN, TOPR_CHANNEL);
+
+  if (ledcSetup(BOTTOML_CHANNEL, PWM_FREQ, PWM_RESOLUTION) == 0)
+  {
+    Serial.println("Failed to setup BOTTOML channel");
+    return -1;
+  }
+  ledcAttachPin(BOTTOML_PIN, BOTTOML_CHANNEL);
+
+  if (ledcSetup(BOTTOMR_CHANNEL, PWM_FREQ, PWM_RESOLUTION) == 0)
+  {
+    Serial.println("Failed to setup BOTTOMR channel");
+    return -1;
+  }
+  ledcAttachPin(BOTTOMR_PIN, BOTTOMR_CHANNEL);
+
+  Serial.println("All motor channels initialized");
+  return 0;
+}
+
+void armMotors()
+{
+  int armDuty = map(1000, 1000, 2000, 3276, 6553);
+
+  ledcWrite(TOPL_CHANNEL, armDuty);
+  ledcWrite(TOPR_CHANNEL, armDuty);
+  ledcWrite(BOTTOML_CHANNEL, armDuty);
+  ledcWrite(BOTTOMR_CHANNEL, armDuty);
+
+  Serial.println("Sending arming signal to ESCs...");
+  delay(3000);
+  Serial.println("ESCs armed.");
+}
+
+void infiniteLoop()
+{
+  while (1)
+  {
+    delay(100);
+  }
+}
+
 void setup()
 {
   Serial.begin(115200);
@@ -21,36 +93,32 @@ void setup()
   if (!initRadio())
   {
     Serial.println("Radio failed to init, aborting.");
-    while (1)
-    {
-      delay(1000);
-    }
+    infiniteLoop();
   }
 
   if (!initICM())
   {
     Serial.println("IMU failed to init, aborting.");
-    while (1)
-    {
-      delay(100);
-    }
+    infiniteLoop();
   }
 
   if (!bmp.initBMP())
   {
     Serial.println("BMP failed to init, aborting.");
-    while (1)
-    {
-      delay(100);
-    }
+    infiniteLoop();
   }
   if (!ads.initADS())
   {
     Serial.println("ADS failed to init, aborting.");
-    while (1)
-    {
-      delay(100);
-    }
+    infiniteLoop();
+  }
+  if (initMotors())
+  {
+    armMotors();
+  }
+  else
+  {
+    infiniteLoop();
   }
 }
 
